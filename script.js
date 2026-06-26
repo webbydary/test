435327264fea166596081c6616cb9217156c3a24
webbydary <webbydary@gmail.com>
2026-06-26 16:29:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-const items = document.querySelectorAll('.fade');
 
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add('show');
-    }
-  });
-}, {
-  threshold: 0.1
-});
-
-items.forEach(el => observer.observe(el));
